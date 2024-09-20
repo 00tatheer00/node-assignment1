0fdc9c89f5b797d24feb9341118a4f4b8b6ccb47
Tatheer Hussain <tatheerabidi0@gmail.com>
2024-09-20 17:47:34 +0500

    get method added

--- index.mjs
+++ index.mjs
@@ -3,6 +3,18 @@ import { URL } from 'url';
 
 const server = createServer((req, res) => {
     const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
+
+    if (req.method === 'GET' && parsedUrl.pathname === '/get') {
+        const data = [
+          { name: 'John', age: 30 },
+          { name: 'Jane', age: 25 },
+          { name: 'Bob', age: 40 },
+        ];
+        const jsonData = JSON.stringify(data);
+        
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(jsonData);
+      }
 });
 
 server.listen(3000, () => {
