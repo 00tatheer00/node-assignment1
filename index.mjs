6ec1e6c079f30aa7d881721e267c7648e66e49aa
Tatheer Hussain <tatheerabidi0@gmail.com>
2024-09-20 17:39:00 +0500

    server created

--- index.mjs
+++ index.mjs
@@ -2,5 +2,4 @@ import { createServer } from 'http';
 import { URL } from 'url';
 
 const server = createServer((req, res) => {
-    const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
-});
+  const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
