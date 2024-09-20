9835fc65247d56746ff08a6da9d720386e3e19f0
Tatheer Hussain <tatheerabidi0@gmail.com>
2024-09-20 17:41:20 +0500

    server created

--- index.mjs
+++ index.mjs
@@ -2,5 +2,9 @@ import { createServer } from 'http';
 import { URL } from 'url';
 
 const server = createServer((req, res) => {
-  const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
+    const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
+});
+
+server.listen(3000, () => {
+    console.log('Server running on port 3000');
 });
